a2b61b1d58d9542f3d41eec0608e0db608ac3500
资深老萌新 <2580815781@qq.com>
2024-02-15 17:20:58 +0800

    init:初始化添加三个子模块

---
+++ admin/src/main/java/com/kiro/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,16 @@
+package com.kiro.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kiro
+ * @project shortlink
+ * @time 20240215 17:16
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
