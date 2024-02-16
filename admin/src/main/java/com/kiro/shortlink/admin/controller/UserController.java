b72702dc01788a61441bd9f5048a7b6668b0f327
资深老萌新 <2580815781@qq.com>
2024-02-16 11:47:53 +0800

    refactor:增加统一返回规约Result，内部封装状态码、消息、返回数据等

--- admin/src/main/java/com/kiro/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/kiro/shortlink/admin/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.kiro.shortlink.admin.controller;
 
+import com.kiro.shortlink.admin.convention.result.Result;
 import com.kiro.shortlink.admin.dto.resp.UserRespDTO;
 import com.kiro.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +27,12 @@ public class UserController {
      * @return 用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        if (result == null) {
+            return new Result<UserRespDTO>().setCode("-1").setMessage("用户不存在");
+        } else {
+            return new Result<UserRespDTO>().setCode("0").setData(result).setMessage("查询成功");
+        }
     }
 }
