8155307227e9a98e4ebf8ac8bd1671789935bbfb
资深老萌新 <2580815781@qq.com>
2024-02-18 21:40:21 +0800

    feat:增加mbp自定义填充处理器；引入Redis配置文件；定义Redis缓存常量

---
+++ admin/src/main/java/com/kiro/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,11 @@
+package com.kiro.shortlink.admin.common.constant;
+
+/**
+ * @author Kiro
+ * @project shortlink
+ * @time 20240218 18:34
+ * @desc Redis缓存常量
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER = "short-link:lock_user-register:";
+}
---
+++ admin/src/main/java/com/kiro/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,32 @@
+package com.kiro.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+/**
+ * @author Kiro
+ * @project shortlink
+ * @time 20240218 17:23
+ * @desc 自定义填充处理器
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
