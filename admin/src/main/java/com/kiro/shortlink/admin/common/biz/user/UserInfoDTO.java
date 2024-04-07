4e55b75e3099350b2074a02ae749bb0d6fbe37c3
资深老萌新 <2580815781@qq.com>
2024-04-07 21:03:26 +0800

    feat: 更新错误码；增加用户信息传输实体类；封装用户信息上下文

---
+++ admin/src/main/java/com/kiro/shortlink/admin/common/biz/user/UserInfoDTO.java
@@ -0,0 +1,36 @@
+package com.kiro.shortlink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Kiro
+ * @project shortlink
+ * @time 20240403 20:27
+ * @desc 用户信息实体 DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    @JSONField(name = "id")
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+}
